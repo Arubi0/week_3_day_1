0d1617b9ef9cfd15014a0657c1fc3fbff814eb9a
Rubio Jr. <arubiojr@cps.k12.il.us>
2024-09-10 09:23:38 -0500

    f strings concatenation

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -50,20 +50,37 @@
 # # Review
 # create variables for the following :
 # 1. age
+age = 17 # integer variable
 # 2. name
+name = "Armando" # string variable
 # 3. song
+song = "Happy Birthday" # string variable
 # 4. food
+food = "Pizza" # string variable
 # 5. number
-
+number = 1738 # integer variable
 
 # #now include the variables you just made print in the following...
 
 
 # Once upon a time, there was a [age] old coder named [name].
+#concatenation --- + around your variables
+print ("Once upon a time, there was a " + str(age) + " old coder name " + name + ".")
+print ("There was a number" + str(number) + "as well")
 
+#put the age and number in a sentence
+print ("There was a man named " + name + " he had " + str(number) + " dollars in his bank account.")
 
+date_of_birth = 2021
+number2 = 123
+number3 = 123.450
+number4 = 123.33
+number5 = 4555
+# print("A kid was born in the year of " + str(date_of_birth) + "he was trying to learn his numbers. He learned " + str(number2) + " he tried to type it in a computer and mistakenly wrote " + str(number3) + "he did it again by typing" + str(number4) + "he gave up when he wrote" + str(number5))
+# #f strings 
+print(f"the date of birth is {date_of_birth} and the number is {number2} and the number is {number3} and the number is {number4} and the number is {number5} ")
 # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
-
+print(f"{name} liked to hum the {song} while coding. It was so annoying that their teammates would throw {food} until {name} would stop singing.")
 
 # Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
 ##########################################################################################
