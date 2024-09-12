e8f63fa352414579d099d92dc6ef5622527be681
Rubio Jr. <arubiojr@cps.k12.il.us>
2024-09-12 09:25:58 -0500

    Did the Cal python challenge

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -212,7 +212,7 @@
 # # #ceil method
 # print(ceil(5.2))
 # # #sqrt method
-# print(sqrt())
+# print(sqrt(4))
 
 
 
@@ -229,25 +229,41 @@
 ##########################################################################################
 # So what have we learned? We learned some of the basics of numbers in Python. We also learned how to do arithmetic and use Python as a basic calculator. We then wrapped it up with learning about Variable Assignment in Python.
 # # **Getting Input from users**
-# #how do we get input from users?
-# input("what is your name?")
-name = input("what is your name? ")
-print("hello",name)
-# # basic math calculator
-# #ask the user for 2 numbers
-num1 = int(input("Enter a nmuber:"))
-num2 = int(input("Enter another number:"))
-# # print out a statement where you:
-# # add them together
-print(num1 + num2)
-# #multiply
-print(num1 * num2)
-# # find the max number
-print(max(num1,num2))
-# # find the remainder of the numbers
-print(num1%num2)
-# #round one number
-print(round(num1))
+# # #how do we get input from users?
+# # input("what is your name?")
+# name = input("what is your name? ")
+# print("hello",name)
+# # # basic math calculator
+# # #ask the user for 2 numbers
+# num1 = int(input("Enter a nmuber:"))
+# num2 = int(input("Enter another number:"))
+# # # print out a statement where you:
+# # # add them together
+# print(num1 + num2)
+# # #multiply
+# print(num1 * num2)
+# # # find the max number
+# print(max(num1,num2))
+# # # find the remainder of the numbers
+# print(num1%num2)
+# # #round one number
+# print(round(num1))
+
+num1 = int(input("Pick a Number: "))
+num2 = int(input("Pick a Another Number:"))
+print(num1 - num2)
+print(num1/num2)
+print(min(num1,num2))
+print(abs(num1))
+print(abs(num2))
+from math import*
+print(floor(num1))
+print(floor(num2))
+print(ceil(num1))
+print(ceil(num2))
+print(sqrt(num1))
+print(sqrt(num2))
+
 
 
 
